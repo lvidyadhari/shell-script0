80418c8d922933bb629d70f52510689948209653
lvidyadhari <lathavidyadhari97@gmail.com>
2023-12-16 23:02:31 +0530

    added second script

---
+++ variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+$PERSON1 =RAM
+$PERSON2 =RAHEEM
+
+echo "$PEROSN1 : Hello $PERSON2, Good morning"
+echo "$PERSON2 : Hello $PERSON1, morning have a nice day"
+echo "$PERSON1 : Thank you $PERSON2, whis you the same."
